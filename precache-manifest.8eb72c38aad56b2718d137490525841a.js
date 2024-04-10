b95da76a5a7aebbe2b17b4a6dd377fd2de7a52c7
9319kajalrani <166407021+9319kajalrani@users.noreply.github.com>
2024-04-10 20:12:44 +0530

    Updates

---
+++ precache-manifest.8eb72c38aad56b2718d137490525841a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145e214b09119720f0e6a84a375d9f62",
+    "url": "/textcoverter/index.html"
+  },
+  {
+    "revision": "c17f8b4033e61a60db87",
+    "url": "/textcoverter/static/css/main.39ccdd9c.chunk.css"
+  },
+  {
+    "revision": "09347da7ed89c0c04b69",
+    "url": "/textcoverter/static/js/2.59c32d8e.chunk.js"
+  },
+  {
+    "revision": "ba0bfa8f95d9766194b5",
+    "url": "/textcoverter/static/js/3.7426b29a.chunk.js"
+  },
+  {
+    "revision": "c17f8b4033e61a60db87",
+    "url": "/textcoverter/static/js/main.96798012.chunk.js"
+  },
+  {
+    "revision": "a1ecececd310f009cadf",
+    "url": "/textcoverter/static/js/runtime~main.6c869e4b.js"
+  }
+]);
